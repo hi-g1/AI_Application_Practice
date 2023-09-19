202818cdb40b8ab9dbf07c273a4f3b73eaa27256
JJBong <jubong1992@gmail.com>
2023-09-19 19:12:07 +0900

    ...

---
+++ 01.gym/c_gym_atari_env_info.py
@@ -0,0 +1,32 @@
+import gymnasium as gym; print(f"gym.__version__: {gym.__version__}")
+import numpy as np
+np.set_printoptions(edgeitems=3, linewidth=100000, formatter=dict(float=lambda x: "%7.3f" % x))
+
+# https://gymnasium.farama.org/environments/atari/breakout/
+env = gym.make("ALE/Breakout-v5", render_mode="human")
+
+# Num of training episodes = 3
+for episode in range(3):
+    # Reset
+    observation, info = env.reset(seed=123, options={})
+
+    done = False
+    episode_reward = 0.0
+    step = 0
+    while not done:
+        # Agent policy that uses the observation and info
+        action = env.action_space.sample()
+        next_observation, reward, terminated, truncated, info = env.step(action)
+        step += 1
+
+        print("Step: {0}, Obs.: {1}, Action: {2}, Next Obs.: {3}, Reward: {4:5.2f}, Terminated: {5}, Truncated: {6}, Info: {7}".format(
+            step, observation, action, next_observation, reward, terminated, truncated, info
+        ))
+
+        episode_reward += reward
+        observation = next_observation
+        done = terminated or truncated
+
+    print(f"Episode: {episode + 1}, Episode Reward: {episode_reward}")
+
+env.close()
